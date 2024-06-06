20d88064eb019a678f8f344f0c6ecade17ad8e7b
lcknp <knapp.luca@gmx.de>
2024-06-06 22:54:51 +0200

    06.06.2024

--- masonry.js
+++
@@ -1,8 +0,0 @@
-var grid = document.querySelector('.gallery');
-
-var masonry = new Masonry( grid, {
-    itemSelector: '.gallery-item',
-    columnWidth: '.gallery-sizer',
-    gutter: 15, // Abstand zwischen den Elementen
-    fitWidth: true // Richte die Gitterbreite am übergeordneten Container aus
-});
